2ac8564d6e905c92715472ac4dc9a458b7f18941
Santiago Pirela <s17manutd@gmail.com>
2016-09-01 16:29:41 -0400

    F1 Feeder app completed - Installed Angular Materiall
    Completed the feeder app and installed angular material via npm. This is the checkpoint to start applying angular material to the app

---
+++ js/apps/app.js
@@ -0,0 +1,11 @@
+angular.module('F1FeederApp', [
+  'F1FeederApp.services',
+  'F1FeederApp.controllers',
+  'ngRoute'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+	when("/drivers", {templateUrl: "views/drivers.html", controller: "driversController"}).
+	when("/drivers/:id", {templateUrl: "views/driver.html", controller: "driverController"}).
+	otherwise({redirectTo: '/drivers'});
+}]);
